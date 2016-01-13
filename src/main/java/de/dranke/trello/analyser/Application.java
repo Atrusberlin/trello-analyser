01ae1e00dd5b828799f3bd8ff884b5182e98bb19
Daniel Ranke <atrusberlin@gmail.com>
2016-01-13 21:46:50 +0100

    OAuth für Backend und JSFrontend als Spike umgesetzt

--- src/main/java/de/dranke/trello/analyser/Application.java
+++ src/main/java/de/dranke/trello/analyser/Application.java
@@ -1,29 +1,48 @@
 package de.dranke.trello.analyser;
 
+import org.glassfish.jersey.client.oauth1.AccessToken;
 import org.glassfish.jersey.client.oauth1.ConsumerCredentials;
 import org.glassfish.jersey.client.oauth1.OAuth1AuthorizationFlow;
 import org.glassfish.jersey.client.oauth1.OAuth1ClientSupport;
+import org.slf4j.Logger;
 import spark.Spark;
+
+import java.io.File;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static spark.SparkBase.externalStaticFileLocation;
 
 /**
  * Created by Daniel Ranke on 12.01.2016.
  */
 public class Application {
 
-    public static final String callbackURL = "/createSession";
     private static OAuth1AuthorizationFlow flow;
+    public static final Logger LOGGER = getLogger("Application");
+    public static final String APP_PATH = "/trello/analyse";
+    public static final String callbackURL = "http://" +
+            System.getProperty("server.domain") + ":" +
+            System.getProperty("server.port") + APP_PATH + "/createSession";
 
     public static void main(String[] args) {
 
-        Spark.get("/login", (req, res) -> {
-            flow = login();
+        String www = new File("www").getAbsolutePath();
+        LOGGER.info("Read static files from: {}", www);
+        externalStaticFileLocation(www);
+
+        Spark.get(APP_PATH + "/login", (req, res) -> {
+            flow = createFlow();
             res.redirect(flow.start());
             return "";
         });
 
+        Spark.get(APP_PATH + "/createSession", (req, res) -> {
+            return createtClientSession(req.queryParams("oauth_verifier")).getToken();
+        });
+
     }
 
-    public static OAuth1AuthorizationFlow login() {
+    public static OAuth1AuthorizationFlow createFlow() {
         ConsumerCredentials consumerCredentials = new ConsumerCredentials(System.getProperty("trello.key"), System.getProperty("trello.secret"));
         OAuth1AuthorizationFlow flow = OAuth1ClientSupport.builder(consumerCredentials)
                 .authorizationFlow(
@@ -35,10 +54,11 @@ public class Application {
         return flow;
     }
 
-//    public void createtClientSession(String oauthVerifier) {
-//        accessToken = this.authFlow.finish(oauthVerifier);
+    private static AccessToken createtClientSession(String oauthVerifier) {
+        AccessToken accessToken = flow.finish(oauthVerifier);
 //        client = ClientBuilder.newBuilder()
 //                .register(this.authFlow.getOAuth1Feature())
 //                .build();
-//    }
+        return accessToken;
+    }
 }
